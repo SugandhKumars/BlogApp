947ac85404a716404c668f3509c243910f5a895f
SugandhKumars <merrimarrzi@gmail.com>
2024-08-04 23:09:07 +0530

    Backend

--- blogAppBackend/index.js
+++ blogAppBackend/index.js
@@ -1,58 +1,56 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
 const app = express();
-const db = [
-  {
-    name: "learn-js",
-    heading: "Learn JavaScript",
-    upvote: 0,
-    comments: [],
-    Content:
-      "loremdfnsdnfkjndskjfnksdnfjklanslnalksndlkasnflkdnanfnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn",
-  },
-  {
-    name: "learn-node",
-    heading: "Learn Node",
-    upvote: 0,
-    comments: [],
-    Content:
-      "loremdfnsdnfkjndskjfnksdnfjklanslnalksndlkasnflkdnanfnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn",
-  },
-  {
-    name: "learn-mongo",
-    heading: "Learn Database with Mongodb",
-    upvote: 0,
-    comments: [],
-    Content:
-      "loremdfnsdnfkjndskjfnksdnfjklanslnalksndlkasnflkdnanfnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn",
-  },
-];
-app.use(express.json);
-app.get("/api/blog/:name", (req, res) => {
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow only your frontend's origin
+  })
+);
+app.get("/api/blog/:name", async (req, res) => {
   const { name } = req.params;
-  const blog = db.find((article) => article.name == name);
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+  const db = client.db("blogApp");
+  const blog = await db.collection("blogs").findOne({ name });
+  if (blog) {
+    res.json({ blog });
+  }
+});
+app.put("/api/blog/:name/upvote", async (req, res) => {
+  const { name } = req.params;
+  const client = new MongoClient("mongodb://127.0.0.1:27017/");
+  await client.connect();
+  const db = client.db("blogApp");
+  await db.collection("blogs").updateOne(
+    { name },
+    {
+      $inc: { upvote: 1 },
+    }
+  );
+  const blog = await db.collection("blogs").findOne({ name });
   if (blog) {
     res.json(blog);
   } else {
     res.status(404).send("Blog not found");
   }
 });
-app.put("/api/blog/:name/upvote", (req, res) => {
+app.post("/api/blog/:name/comment", async (req, res) => {
   const { name } = req.params;
-  const blog = db.find((article) => article.name == name);
+  const { user, text } = req.body;
+  const client = new MongoClient("mongodb://127.0.0.1:27017/");
+  await client.connect();
+  const db = client.db("blogApp");
+  await db.collection("blogs").updateOne(
+    { name },
+    {
+      $push: { comments: { user, text } },
+    }
+  );
+  const blog = await db.collection("blogs").findOne({ name });
   if (blog) {
-    blog.upvote++;
-    res.send(`the blog ${name} has ${blog.upvote} upvote`);
-  } else {
-    res.status(404).send("Blog not found");
-  }
-});
-app.post("/api/blog/:name/comment", (req, res) => {
-  const { name } = req.params;
-  const { username, text } = req.body;
-  const blog = db.find((article) => article.name == name);
-  if (blog) {
-    blog.comments.push({ username, text });
-    res.json(blog.comments);
+    res.send(blog.comments);
   } else {
     res.status(404).send("Blog not found");
   }
